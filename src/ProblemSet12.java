83005f7ec6fec8a1d1470ed2f200521586e26a11
Jonah Leibowitz <jonahleibowitz@comcast.net>
2021-02-27 13:03:43 -0500

    finished ex1

--- src/ProblemSet12.java
+++ src/ProblemSet12.java
@@ -1,45 +1,70 @@
 import java.util.ArrayList;
 
 public class ProblemSet12 {
+    public static void main(String[] args) {
+        int n = 3;
+        //int end = 16;
+     /*   ArrayList<Integer> numbers = new ArrayList<Integer>();
 
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(7);
+        numbers.add(2);
+        numbers.add(1);
+        numbers.add(7);
 
-    public long factorial(int n) {
+        ArrayList<Integer> inner = new ArrayList<Integer>();
+        inner.add(2);
+        inner.add(4);
+*/
+        System.out.println(factorial(n));
+        //(Arrays.toString(
+    }
+
+    public static long factorial(int n) {
+    if (n < 0){return -1;}
+    long product = 1;
+    long factCount = n;
+    while(factCount > 0){
+      product *= factCount;
+      factCount --;
+    }
+    return product;
+    }
+
+    public static long fibonacci(int n) {
 
     }
 /*
-    public long fibonacci(int n) {
+    public static long triangle(int rows) {
 
     }
 
-    public long triangle(int rows) {
+    public static long sumDigits(int n) {
 
     }
 
-    public long sumDigits(int n) {
+    public static long powerN(int base, int exponent) {
 
     }
 
-    public long powerN(int base, int exponent) {
+    public static String changeXY(String text) {
 
     }
 
-    public String changeXY(String text) {
+    public static int array11(int[] data, int index) {
 
     }
 
-    public int array11(int[] data, int index) {
+    public static int strCount(String text, String sub) {
 
     }
 
-    public int strCount(String text, String sub) {
+    public static boolean strCopies(String text, String sub, int n) {
 
     }
 
-    public boolean strCopies(String text, String sub, int n) {
-
-    }
-
-    public int strDist(String text, String sub) {
+    public static int strDist(String text, String sub) {
 
     }*/
 }
